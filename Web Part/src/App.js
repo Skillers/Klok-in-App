123606ad3ce10c3f3e2d7f4b74a802b92adc01bb
Joris van Breugel <jorisvanbreugel@Joriss-MacBook-Air.local>
2022-06-01 13:15:23 +0200

    Use Hashrouter, so linking works in static site hosting

--- Web Part/src/App.js
+++ Web Part/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/*" element={<Navigate to="/login" replace />} />
       </Routes>
     </div>
   );
